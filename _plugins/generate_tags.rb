972c0e46f7d16bf464a8a288addeb5d52afde5eb
Dr. Drake Vellaut <drake.vellaut@xenolife.org>
2025-08-06 15:34:34 -0500

    recommit

---
+++ _plugins/generate_tags.rb
@@ -0,0 +1,28 @@
+# _plugins/generate_tags.rb
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.each do |tag, posts|
+        site.pages << TagPage.new(site, site.source, tag)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, tag)
+      @site = site
+      @base = base
+      @dir  = "tag/#{Jekyll::Utils.slugify(tag)}"
+      @name = "index.html"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_page.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Posts tagged with #{tag}"
+      self.data['tag_posts'] = site.tags[tag].sort_by { |p| p.date }.reverse
+      File.write(File.join(site.dest, "plugin-ran.txt"), "Tag plugin executed successfully.")
+    end
+  end
+end
