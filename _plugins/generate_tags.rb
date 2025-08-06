2b5e929ccf49551d85a958a7286cf1f9c2d456cc
Dr. Drake Vellaut <drake.vellaut@xenolife.org>
2025-08-06 14:22:06 -0500

    generate tags

---
+++ _plugins/generate_tags.rb
@@ -0,0 +1,26 @@
+# _plugins/generate_tags.rb
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.each do |tag, posts|
+        site.pages << TagPage.new(site, site.source, tag)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, tag)
+      @site = site
+      @base = base
+      @dir  = "tag/#{tag}"
+      @name = "index.html"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Posts tagged with #{tag}"
+    end
+  end
+end
