200a7cb4cb69091b1ae5798b16db132a068f8ca0
Dr. Drake Vellaut <drake.vellaut@xenolife.org>
2025-08-06 14:34:46 -0500

    generate tags

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -18,7 +18,7 @@ module Jekyll
       @name = "index.html"
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.read_yaml(File.join(base, '_layouts'), 'tag_page.html')
       self.data['tag'] = tag
       self.data['title'] = "Posts tagged with #{tag}"
     end
